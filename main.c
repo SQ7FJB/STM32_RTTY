1444a68b4a95cc87abf1e393ae50e333efba59b8
Łukasz Nidecki <qyonek+github@gmail.com>
2017-06-06 19:28:37 +0200

    Code cleanup

--- main.c
+++ main.c
@@ -167,8 +167,7 @@ int main(void) {
   radio_enable_tx();
 
   uint8_t rtty_before_aprs_left = RTTY_TO_APRS_RATIO;
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wmissing-noreturn"
+
   while (1) {
     if (tx_on == 0 && tx_enable) {
       if (rtty_before_aprs_left){
@@ -192,7 +191,6 @@ int main(void) {
       __WFI();
     }
   }
-#pragma clang diagnostic pop
 }
 
 void send_rtty_packet() {
